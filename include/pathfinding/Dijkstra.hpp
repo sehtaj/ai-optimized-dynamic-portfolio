93a13ce18c4857f224884d0a1995a0ffa59d06d5
sehtaj <batthsehtaj10@gmail.com>
2024-10-10 21:02:07 -0400

    Dijkstra.hpp added

---
+++ include/pathfinding/Dijkstra.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class DijkstraComparison
+{
+	gbl::Map<float>* distances;
+public:
+	DijkstraComparison(gbl::Map<float>* i_distances = 0);
+
+	bool operator()(const gbl::Position<>& i_left_cell, const gbl::Position<>& i_right_cell) const;
+};
+
+bool dijkstra_search(unsigned short& i_path_length, unsigned short& i_total_checks, std::chrono::microseconds& i_duration, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::priority_queue<gbl::Position<>, std::vector<gbl::Position<>>, DijkstraComparison>& i_path_queue, gbl::Map<float>& i_distances, const gbl::Position<>& i_finish_position, const gbl::Position<>& i_start_position, gbl::Map<>& i_map);
+
+void dijkstra_reset(bool& i_finished, unsigned short& i_path_length, unsigned short& i_total_checks, std::chrono::microseconds& i_duration, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::priority_queue<gbl::Position<>, std::vector<gbl::Position<>>, DijkstraComparison>& i_path_queue, gbl::Map<float>& i_distances, const gbl::Position<>& i_start_position, gbl::Map<>& i_map);
